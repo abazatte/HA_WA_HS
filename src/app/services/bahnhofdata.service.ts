3ba3841cbd16ff987ec62e3b3e094be490b54e71
abazatte <a.azattemuer@gmail.com>
2023-08-19 11:18:49 -0400

    feat: BahnhofPictures sind drin.

--- src/app/services/bahnhofdata.service.ts
+++ src/app/services/bahnhofdata.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Bahnhof } from 'src/custom_type_definition';
+import { BahnhofpictureService } from './bahnhofpicture.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BahnhofdataService {
   bahnhof: Bahnhof | undefined;
-  constructor() { }
+  picture: string = '';
+  responseData: any = {};
+  constructor(private bahnhofpicture: BahnhofpictureService) { }
 
   setBahnhof(currentBahnhof: Bahnhof) {
     this.bahnhof = currentBahnhof;
+    this.setPicture();
   }
 
   barrierefreiheit() {
@@ -17,7 +21,7 @@ export class BahnhofdataService {
   }
   
   parken(){
-
+    
   }
 
   transport(){
@@ -30,5 +34,23 @@ export class BahnhofdataService {
 
   anschrift(){
 
+  }
+
+  setPicture(){
+    this.bahnhofpicture.performGetRequest(this.bahnhof?.number).subscribe(
+      (response) => {
+        //this.responseData.photoBaseUrl
+        this.responseData = response;
+
+        this.picture = this.responseData.photoBaseUrl + this.responseData.stations[0].photos[0].path;
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+
+  getPicture() {
+    return this.picture;
   }
 }
