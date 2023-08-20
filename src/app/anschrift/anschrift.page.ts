096399ad0814c377435295b8aec3e37743c28066
mjaesch <maximilianjaesch@gmail.com>
2023-08-20 10:23:35 -0400

    feat: responsive maps

--- src/app/anschrift/anschrift.page.ts
+++ src/app/anschrift/anschrift.page.ts
@@ -13,13 +13,14 @@ import apikeys from '../../../apikeys.json';
 export class AnschriftPage implements OnInit {
   safeSrc: SafeResourceUrl;
   srcString: string = "https://www.google.com/maps/embed/v1/place?key=" +apikeys[0]["api-key"];
-
+  public screenHeightCustom: number = 400;
 
   constructor(private checkSession: ChecksessionService, private bahnhofDataService: BahnhofdataService,private sanitizer: DomSanitizer) { 
     if(this.bahnhofDataService.anschrift()){
       this.srcString += this.bahnhofDataService.anschrift();
     }
     this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl(this.srcString);
+    this.screenHeightCustom = Math.floor(window.innerHeight * 0.80);
   }
 
   ngOnInit() {
