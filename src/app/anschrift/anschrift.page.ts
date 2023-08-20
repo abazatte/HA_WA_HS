a4140bb218925d4dabe92b75945cfd00b77aa3f8
mjaesch <maximilianjaesch@gmail.com>
2023-08-20 09:13:52 -0400

    feat: google maps an den richtigen ort

--- src/app/anschrift/anschrift.page.ts
+++ src/app/anschrift/anschrift.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ChecksessionService } from '../services/checksession.service';
 import { BahnhofdataService } from '../services/bahnhofdata.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import apikeys from '../../../apikeys.json';
+
 
 @Component({
   selector: 'app-anschrift',
@@ -8,8 +11,14 @@ import { BahnhofdataService } from '../services/bahnhofdata.service';
   styleUrls: ['./anschrift.page.scss'],
 })
 export class AnschriftPage implements OnInit {
+  safeSrc: SafeResourceUrl;
+  srcString: string = "https://www.google.com/maps/embed/v1/place?key=" +apikeys[0]["api-key"] + "&q=Osnabrück,49074,Theodor-Heuss-Platz+2" ;
 
-  constructor(private checkSession: ChecksessionService, private bahnhofDataService: BahnhofdataService) { }
+
+  constructor(private checkSession: ChecksessionService, private bahnhofDataService: BahnhofdataService,private sanitizer: DomSanitizer) { 
+    this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl(this.srcString);
+
+  }
 
   ngOnInit() {
     this.checkSession.checkIfNotLoggedIn();
