10a5489c000657b6462340289ca741cfcc1168b8
mjaesch <maximilianjaesch@gmail.com>
2023-08-20 09:43:09 -0400

    feat: custom addresse

--- src/app/anschrift/anschrift.page.ts
+++ src/app/anschrift/anschrift.page.ts
@@ -12,12 +12,14 @@ import apikeys from '../../../apikeys.json';
 })
 export class AnschriftPage implements OnInit {
   safeSrc: SafeResourceUrl;
-  srcString: string = "https://www.google.com/maps/embed/v1/place?key=" +apikeys[0]["api-key"] + "&q=Osnabrück,49074,Theodor-Heuss-Platz+2" ;
+  srcString: string = "https://www.google.com/maps/embed/v1/place?key=" +apikeys[0]["api-key"];
 
 
   constructor(private checkSession: ChecksessionService, private bahnhofDataService: BahnhofdataService,private sanitizer: DomSanitizer) { 
+    if(this.bahnhofDataService.anschrift()){
+      this.srcString += this.bahnhofDataService.anschrift();
+    }
     this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl(this.srcString);
-
   }
 
   ngOnInit() {
